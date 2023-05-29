8bbe87b8848539833514507bd281644bfcd45565
andreoluiz <andrecardosoal99@gmail.com>
2023-05-29 10:04:06 -0300

    Criação dos blocos

--- Main.java
+++ Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+import bc.Block;
+import bc.Store;
+
+public class Main {
+
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+
+        for(int i = 1; i < Store.blockchain.size(); i++){
+            currentBlock = Store.blockchain.get(i);
+            previousBlock = Store.blockchain.get(i-1);
+
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+
+            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
